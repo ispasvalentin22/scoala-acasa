630fd6ed77b5606586cb3240b8537f05e9d89ee3
Valentin Ispas <51799997+ispasvalentin22@users.noreply.github.com>
2021-03-13 16:11:06 +0200

    update dashboard styling

--- client/src/pages/dashboard/dashboard.component.jsx
+++ client/src/pages/dashboard/dashboard.component.jsx
@@ -4,16 +4,36 @@ import { getUserInfo } from '../../redux/user/user.actions';
 
 const Dashboard = () => {
   return (
-    <section className="sidemenu">
-      <h2 className="sidemenu__app">Scoala acasa</h2>
-      <div className="currentuser">
-        <i class="fas fa-user-circle"></i>
-        <div className="currentuser__details">
-          <h2>Current user</h2>
-          <h3>Cadru didactic</h3>
+    <div className="dashboard-wrapper">
+      <section className="sidemenu">
+        <h2 className="sidemenu__logo">Scoala acasa</h2>
+        <div className="currentuser">
+          <i class="fas fa-user-circle"></i>
+          <div className="currentuser__details">
+            <h2>Current user</h2>
+            <h3>Cadru didactic</h3>
+          </div>
         </div>
-      </div>
-    </section>
+        <div className="class-activity">
+          <h3 className="class-activity-title">ACTIVITATEA LA CLASĂ</h3>
+          <div className="sidemenu__homepage">
+            <i class="fas fa-border-all"></i>
+            <h3 className="sidemenu__homepage--h3">Pagina principală</h3>
+          </div>
+          <div className="sidemenu__catalogue">
+            <i class="far fa-clipboard"></i>
+            <h3 className="sidemenu__catalogue--title">Catalog</h3>
+          </div>
+        </div>
+      </section>
+
+      <main className="sidemenu__main">
+        <div className="miniheader">
+          
+        </div>
+      </main>
+    </div>
+
   )
 }
 
