099fb46057d5f952bcb2db23c135a39a70f0c595
Valentin Ispas <51799997+ispasvalentin22@users.noreply.github.com>
2021-03-16 21:24:46 +0200

    added dashboard menu and dashboard first page component

--- client/src/pages/dashboard/dashboard.component.jsx
+++ client/src/pages/dashboard/dashboard.component.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserInfo } from '../../redux/user/user.actions';
 import { isLoggedIn } from "../../utils/isLoggedIn";
+
+import DashboardMenu from '../../components/dashboard-menu/dashboard-menu.component';
+import DashboardAnnouncements from '../../components/dashboard-announcements/dashboard-announcements.component';
 
 const Dashboard = () => {
   useSelector((state) => state.user);
@@ -24,7 +28,12 @@ const Dashboard = () => {
   // }, [dispatch]);
 
   return (
-
+    <div className="dashboard-container">
+      <DashboardMenu />
+        <Switch>
+          <Route path="/dashboard" exact component={() => <DashboardAnnouncements /> } />
+        </Switch>
+    </div>
   );
 }
 
