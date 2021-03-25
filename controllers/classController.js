cf1f84b842408e70adb5414d0f63cbac55a21e0e
Valentin Ispas <51799997+ispasvalentin22@users.noreply.github.com>
2021-03-25 17:07:24 +0200

    added class controller

--- controllers/classController.js
+++ controllers/classController.js
@@ -1,4 +1,72 @@
 const Class = require('./../models/classModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
-exports.getAllClasses
+exports.getAllClasses = catchAsync(async (req, res, next) => {
+  const classes = await Class.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: classes.length,
+    data: {
+      classes,
+    },
+  });
+});
+
+exports.getClass = catchAsync(async (req, res, next) => {
+  const classs = await Class.findById(req.params.id);
+
+  if (!classs) {
+    return next(new AppError('No class found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      classs,
+    },
+  });
+});
+
+exports.createClass = catchAsync(async (req, res, next) => {
+  const classs = await Class.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      classs,
+    },
+  });
+});
+
+exports.deleteClass = catchAsync(async (req, res, next) => {
+  const classs = await Class.findByIdAndDelete(req.params.id);
+
+  if (!classs) {
+    return next(new AppError('No class found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+exports.updateClass = catchAsync(async (req, res, next) => {
+  const classs = await Class.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!classs) {
+    return next(new AppError('No class found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: classs,
+    },
+  });
+});
