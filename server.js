8b95b914411a48e6920da64b7beac57f405774e8
Valentin Ispas <51799997+ispasvalentin22@users.noreply.github.com>
2021-04-07 18:29:12 +0300

    fix

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+const express = require('express');
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
