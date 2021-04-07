d03a769f27f3e4ae76f27ca1743d0ec6ec70e342
Valentin Ispas <51799997+ispasvalentin22@users.noreply.github.com>
2021-04-07 22:02:54 +0300

    fixN

--- server.js
+++ server.js
@@ -30,9 +30,8 @@ if (
   process.env.NODE_ENV === 'production' ||
   process.env.NODE_ENV === 'staging'
 ) {
-  app.use(express.static(__dirname + '/client/build'));
-
   const path = require('path');
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
