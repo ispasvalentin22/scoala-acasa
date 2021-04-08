13c7f2b3fe9955b4f616f0a6e090cee40cab467d
Valentin Ispas <51799997+ispasvalentin22@users.noreply.github.com>
2021-04-08 16:09:12 +0300

    fix x

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 
 
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './build'))
+    res.sendFile(path.join(__dirname, './client/build'))
 });
 
 const port = process.env.PORT || 4000;
