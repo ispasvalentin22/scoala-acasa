3338d65875be9fe686f78db156b3d860c8e8084d
Valentin Ispas <51799997+ispasvalentin22@users.noreply.github.com>
2021-04-08 16:39:24 +0300

    fix

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -6,7 +6,7 @@ const setAuthHeaderValue = () => {
 };
 
 const axiosInstance = axios.create(
-  { baseURL: 'http://localhost:4000' },
+  { baseURL: 'https://localhost:4000' },
   {
     headers: {
       'Content-Type': 'application/json',
--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use(
   '/api',
-  createProxyMiddleware({ target: 'https://localhost:4000', changeOrigin: true })
+  createProxyMiddleware({
+    target: 'https://localhost:4000',
+    changeOrigin: true,
+  })
 );
 
 const DB = process.env.DATABASE.replace(
@@ -25,11 +28,10 @@ mongoose
   })
   .then(() => console.log('DB connection successful!'));
 
-
 app.use(express.static(path.join(__dirname, './client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build'))
+  res.sendFile(path.join(__dirname, './client/build'));
 });
 
 const port = process.env.PORT || 4000;
